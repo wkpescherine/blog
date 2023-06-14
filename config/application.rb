58ad52df44e392f8ea1b828688a2b81e42286113
wkpescherine <wkpescherine@gmail.com>
2023-06-13 22:35:54 -0400

    Commiting to branch

---
+++ config/application.rb
@@ -0,0 +1,22 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Blog
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.2
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+  end
+end
+
+/usr/local/lib/ruby/gems/3.0.0/bin
+echo 'export PATH="/usr/local/opt/ruby/bin:$PATH"' >> /Users/wkpescherine/.bash_profile
